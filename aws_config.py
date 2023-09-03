044161589ec4e6efe4f045c28ffe97742a546d10
raketbizdev <radon1284@yahoo.com>
2023-09-03 22:51:46 +0800

    update

--- aws_config.py
+++ aws_config.py
@@ -4,3 +4,28 @@ AWS_SECRET_ACCESS_KEY = 'YOUR_SECRET_ACCESS_KEY'
 AWS_REGION = 'YOUR_REGION'
 BUCKET_NAME = 'company-x'
 DIRECTORY_TO_WATCH = 'company-file'
+
+# Function to validate AWS constants
+def validate_aws_config():
+  errors = []
+  
+  if not AWS_ACCESS_KEY_ID or AWS_ACCESS_KEY_ID == 'YOUR_ACCESS_KEY_ID':
+    errors.append("AWS_ACCESS_KEY_ID is missing or not configured.")
+    
+  if not AWS_SECRET_ACCESS_KEY or AWS_SECRET_ACCESS_KEY == 'YOUR_SECRET_ACCESS_KEY':
+    errors.append("AWS_SECRET_ACCESS_KEY is missing or not configured.")
+    
+  if not AWS_REGION or AWS_REGION == 'YOUR_REGION':
+    errors.append("AWS_REGION is missing or not configured.")
+    
+  if not BUCKET_NAME or BUCKET_NAME == 'company-x':
+    errors.append("BUCKET_NAME is missing or not configured.")
+    
+  if not DIRECTORY_TO_WATCH or DIRECTORY_TO_WATCH == 'company-file':
+    errors.append("DIRECTORY_TO_WATCH is missing or not configured.")
+    
+  if errors:
+    raise Exception("\n".join(errors))
+
+# Call the function to validate
+validate_aws_config()
--- main.py
+++ main.py
@@ -4,15 +4,24 @@ from file_watcher import Watcher
 from s3_uploader import S3Uploader
 from logging_config import LOGGING_FORMAT, LOGGING_DATE_FORMAT, LOGGING_LEVEL, LOG_FILE
 
+# Initialize logging
 logging.basicConfig(filename=LOG_FILE, level=LOGGING_LEVEL, format=LOGGING_FORMAT, datefmt=LOGGING_DATE_FORMAT)
 
+# Validate AWS Configuration
+try:
+  import aws_config  # This will run validate_aws_config() and may raise an exception
+except Exception as e:
+  print(f"Error in AWS Configuration: {e}")
+  logging.error(f"Error in AWS Configuration: {e}")
+  exit(1)
+
 if __name__ == '__main__':
-    logging.info("Starting S3Syncer")
-    s3_uploader = S3Uploader()
-    watcher = Watcher(s3_uploader=s3_uploader)
-    watcher.run()
-    try:
-        while True:
-            time.sleep(5)
-    except KeyboardInterrupt:
-        logging.info("Observer Stopped")
+  logging.info("Starting S3Syncer")
+  s3_uploader = S3Uploader()
+  watcher = Watcher(s3_uploader=s3_uploader)
+  watcher.run()
+  try:
+    while True:
+      time.sleep(5)
+  except KeyboardInterrupt:
+    logging.info("Observer Stopped")
